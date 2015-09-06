6c7ad286d172b7cc1d2dc011b5686517f135cd70
shoma2da <marcos.shoichi.matsuda@gmail.com>
2015-09-06 16:32:20 +0900

    Create test classes

---
+++ android-youtube-data-api-client/src/test/java/io/github/shoma2da/android_youtube_data_api_client/api/SearchResultItemTest.java
@@ -0,0 +1,18 @@
+package io.github.shoma2da.android_youtube_data_api_client.api;
+
+import static junit.framework.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Created by shoma2da on 15/09/06.
+ */
+public class SearchResultItemTest {
+
+    @Test
+    public void CreateObject() {
+        SearchResultItem item = new SearchResultItem();
+        assertNotNull(item);
+    }
+
+}
---
+++ android-youtube-data-api-client/src/test/java/io/github/shoma2da/android_youtube_data_api_client/api/SearchResultListTest.java
@@ -0,0 +1,18 @@
+package io.github.shoma2da.android_youtube_data_api_client.api;
+
+import static junit.framework.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Created by shoma2da on 15/09/06.
+ */
+public class SearchResultListTest {
+
+    @Test
+    public void CreateObject() {
+        SearchResultList list = new SearchResultList();
+        assertNotNull(list);
+    }
+
+}
